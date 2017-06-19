20127caca2342fb3862d3641eb66a9add93f372b
fepz <franpaez@gmail.com>
2017-06-19 00:03:20 -0300

    Remove line.

--- apps/freertos_blinky/src/blinky.c
+++ apps/freertos_blinky/src/blinky.c
@@ -38,7 +38,6 @@
 #define CRP_NO_CRP          0xFFFFFFFF
 __attribute__ ((used,section(".crp"))) const unsigned int CRP_WORD = CRP_NO_CRP ;
 
-
 /*****************************************************************************
  * Private types/enumerations/variables
  ****************************************************************************/
